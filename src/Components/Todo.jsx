50ff5f0abf8b1de1464e164f2d38563750123721
FSBurn <Rocknteemo@gmail.com>
2022-08-12 18:14:00 +0300

    first commit

---
+++ src/Components/Todo.jsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+
+function Todo() {
+
+    const [count, setCount] = useState(0);
+    const [divArr, setDivArr] = useState([]);
+    console.log(count)
+
+    return (
+        <div>
+            <button onClick={() => (setCount(count + 1), setDivArr(divArr.concat(count + 1)))}>
+                Add
+            </button>
+            <button onClick={() => (setCount(count > 0 ? count - 1 : null), setDivArr({divArr} = divArr.pop()))}>
+                Delete
+            </button>
+
+            <div>{count ? divArr.map((newDiv) => <div key={''}>{newDiv}</div>) : "empty list"}</div>
+
+
+        </div>
+    );
+}
+
+export default Todo;
