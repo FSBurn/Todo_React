eeada24c1cea16cdd6282799c9fe124d1b9247d4
FSBurn <Rocknteemo@gmail.com>
2022-08-13 13:00:23 +0300

    All comments corrected

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -2,27 +2,27 @@ import React, {useState} from 'react';
 
 function Todo() {
 
-    const [divArr, setDivArr] = useState([]);
+    const [cards, setCards] = useState([]);
 
     const addCard = () => {
-        const newArray = [ ...divArr, {title: '11', id: Date.now()}];
-        setDivArr(newArray)
+        const newArray = [...cards,
+            {
+                title: '11',
+                id: Date.now()
+            }];
+        setCards(newArray)
     }
     const removeCard = () => {
-        setDivArr(divArr.filter((element, index) => index < divArr.length - 1))
+        setCards(cards.filter((element, index) => index < cards.length - 1))
     }
     return (
         <div>
-            <button onClick={() => {
-                addCard()
-            }}>Add
+            <button onClick={addCard}>Add
             </button>
-            <button onClick={() => {
-                removeCard()
-            }}>Delete
+            <button onClick={removeCard}>Delete
             </button>
-            <div>{divArr.length ? divArr.map((newDiv, index) => <div
-                key={index}>{index+1}</div>) : "empty list"}</div>
+            <div>{cards.length ? cards.map((newDiv, index) => <div
+                key={index}>{index + 1}</div>) : "empty list"}</div>
         </div>
     );
 }
