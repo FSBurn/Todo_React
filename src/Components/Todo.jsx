6afb9f14eb5d4be40dee5f2d8ef9e974ec968ef2
FSBurn <Rocknteemo@gmail.com>
2022-08-15 16:15:37 +0300

    add form and styles

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,28 +1,39 @@
 import React, {useState} from 'react';
+import "./Todo.css"
 
 function Todo() {
 
     const [cards, setCards] = useState([]);
 
-    const addCard = () => {
+    const addCard = (event) => {
         const newArray = [...cards,
             {
-                title: '11',
+                title: "event.target.value",
                 id: Date.now()
             }];
         setCards(newArray)
+        event.preventDefault()
     }
     const removeCard = () => {
         setCards(cards.filter((element, index) => index < cards.length - 1))
     }
     return (
         <div>
-            <button onClick={addCard}>Add
-            </button>
-            <button onClick={removeCard}>Delete
-            </button>
-            <div>{cards.length ? cards.map((newDiv, index) => <div
-                key={index}>{index + 1}</div>) : "empty list"}</div>
+            <div className="todoContainer">
+            <div className={"padding10"}>{cards.length ? <div>You have {cards.length} Todos</div> : null}</div>
+
+            <div>{cards.length ? cards.map((newDiv, index) => <div className={"todoItem padding10 gray_line"}
+                key={index}>Task: {index + 1}
+                <button className={"card_button"} onClick={removeCard}>X
+                </button>
+            </div>) : "empty list"}</div>
+            <form onSubmit={addCard} className={"todoItem padding10"}>
+                <label>
+                    <input  className={"input_size"} type="text" placeholder={"Enter item"} />
+                </label>
+                <input className={"submit_button"} type="submit" value="Submit" />
+            </form>
+            </div>
         </div>
     );
 }
