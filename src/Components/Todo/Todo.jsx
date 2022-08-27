6a7a8509c6c83316335f821e2b4002d071b99c0f
FSBurn <Rocknteemo@gmail.com>
2022-08-27 14:53:29 +0300

    add try catch and dir Constant

--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -3,13 +3,17 @@ import styles from "./Todo.module.css"
 import TodoHeader from "./TodoHeader/TodoHeader";
 import TodoCard from "./TodoCard/TodoCard";
 import TodoSubmit from "./TodoSubmit/TodoSubmit";
+import {CARDS_KEY} from "../../Constants/localStorageKeys"
 
 export default function Todo() {
-    const [cards, setCards] = useState(() => {
-        const localStorageData = localStorage.getItem("dataKey");
-        return JSON.parse(localStorageData);
-
-    });
+    const [cards, setCards] = useState(localStorage.getItem(CARDS_KEY) ? () => {
+        try {
+            const localStorageData = localStorage.getItem(CARDS_KEY);
+            return JSON.parse(localStorageData);
+        } catch (e) {
+            console.log(e)
+        }
+    } : []);
 
     const addCard = (value) => {
         setCards([...cards, {
@@ -18,12 +22,16 @@ export default function Todo() {
         }])
     };
 
-    useEffect(() => {
-        localStorage.setItem("dataKey", JSON.stringify(cards));
-    }, [cards]);
-
     const removeCard = (event) => {
         setCards(cards.filter(element => element.id !== Number(event.target.id)))
+    }
+
+    try {
+        useEffect(() => {
+            localStorage.setItem(CARDS_KEY, JSON.stringify(cards));
+        }, [cards]);
+    } catch (e) {
+        console.log(e)
     }
 
     return (
---
+++ src/Constants/localStorageKeys.jsx
@@ -0,0 +1,2 @@
+export const CARDS_KEY = "todoCards";
+
